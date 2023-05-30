d8ddcf52a5d7dbf47d82a490c955a4d87b5a77e4
Adans Mashni <adans.mashni@gmail.com>
2023-05-30 20:40:46 -0300

    novo

--- src/app.py
+++ src/app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template
 import dapr.clients
 import os
 
-
-
 app = Flask(__name__)
 dapr_client = dapr.clients.DaprClient()
 
@@ -18,10 +16,29 @@ def executar_sql(declaracao_sql, *parametros):
     )
     return resposta.json()
 
+def sql_output(provedor):
+    with dapr_client:
+        sqlCmd = ('SELECT * FROM providers WHERE nome_provedor = $1', provedor)
+        payload = {'sql': sqlCmd}
+
+        print(sqlCmd, flush=True)
+
+        try:
+            # Insert order using Dapr output binding via HTTP Post
+            resp = dapr_client.invoke_binding(binding_name="postgres-db", operation='exec',
+                                    binding_metadata=payload, data='')
+            return resp
+        except Exception as e:
+            print(e, flush=True)
+            raise SystemExit(e)
+
+
+
 @app.route("/")
 def home():
-    provedor = os.environ.get('PROVEDOR')
-    resultado = executar_sql("SELECT * FROM providers WHERE nome_provedor = $1", provedor)
+    meu_provedor = os.environ.get('PROVEDOR')
+    resultado = sql_output(meu_provedor)
+    #resultado = executar_sql("SELECT * FROM providers WHERE nome_provedor = $1", provedor)
     return render_template("index.html", resultado=resultado)
 
 if __name__ == "__main__":
