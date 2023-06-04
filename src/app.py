d79983e8c36e49ff9c62e5453ea1047bcd5e3b36
Adans Mashni <adans.mashni@gmail.com>
2023-06-03 23:19:21 -0300

    versao para entregar

--- src/app.py
+++ src/app.py
@@ -2,22 +2,8 @@ from flask import Flask, render_template
 import dapr.clients
 import os
 import json
-import logging
-
 
 app = Flask(__name__)
-
-def executar_sql(declaracao_sql, *parametros):
-    dapr_client = dapr.clients.DaprClient()
-    resposta = dapr_client.invoke_binding(
-        binding_name="postgres-db",
-        operation="exec",
-        data={
-            "query": declaracao_sql,
-            "params": parametros
-        }
-    )
-    return resposta.data.json()
 
 def sql_output(provedor):
     dapr_client = dapr.clients.DaprClient()
@@ -32,13 +18,11 @@ def sql_output(provedor):
         except Exception as e:
             print(e, flush=True)
             raise SystemExit(e)
-
   
 @app.route("/")
 def home():
     meu_provedor = os.environ.get('PROVEDOR')
-    #resultado = sql_output(meu_provedor)
-    resultado = executar_sql("SELECT companhia FROM providers WHERE nome_provedor= $1", meu_provedor)
+    resultado = sql_output(meu_provedor)
     return render_template("index.html", resultado=resultado)
 
 if __name__ == "__main__":
